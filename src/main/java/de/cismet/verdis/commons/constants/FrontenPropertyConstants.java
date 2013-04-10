4642c210950c80c8135dc547aa276231f95fc3b0
Gilles Baatz <gilles.baatz@cismet.de>
2013-04-10 16:36:21 +0200

    #2 added the class FrontenPropertyConstants, as it was missing.

---
+++ src/main/java/de/cismet/verdis/commons/constants/FrontenPropertyConstants.java
@@ -0,0 +1,46 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ *  Copyright (C) 2013 gbaatz
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cismet.verdis.commons.constants;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   gbaatz
+ * @version  $Revision$, $Date$
+ */
+public final class FrontenPropertyConstants extends PropertyConstants {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String PROP__FRONT_INFO = "frontinfo";
+    public static final String PROP__KASSENZEICHEN_REFERENCE = "kassenzeichen_reference";
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new FrontinfoPropertyConstants object.
+     */
+    private FrontenPropertyConstants() {
+    }
+}
